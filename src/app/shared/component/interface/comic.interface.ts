b789d9b37904ab4fa24dc91a383414ca6f109647
Andres Rojas <andresrojasjimenez@hotmail.com>
2021-05-22 17:17:11 -0500

    conexion con imagenes

--- src/app/shared/component/interface/comic.interface.ts
+++ src/app/shared/component/interface/comic.interface.ts
@@ -5,13 +5,18 @@ export interface Comic{
     issueNumber:string;
     variantDescription: string;
     descrption:string;
-  urls:Url[]
-    
+    urls?:Url[]
+    images?:Image[]
 
 
 }
 export interface Url{
 type: string;
 url:string;
+
+}
+export interface Image{
+path: string;
+extension:string;
 
 }
